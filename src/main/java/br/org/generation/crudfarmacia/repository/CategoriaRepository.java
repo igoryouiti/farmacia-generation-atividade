210b17b1e1586d466d2c45a7c0ddaf1dba69db56
igoryouiti <igoryouiti@hotmail.com>
2022-08-02 16:32:03 -0300

    Colado @Param no parâmetro

--- src/main/java/br/org/generation/crudfarmacia/repository/CategoriaRepository.java
+++ src/main/java/br/org/generation/crudfarmacia/repository/CategoriaRepository.java
@@ -2,11 +2,11 @@ package br.org.generation.crudfarmacia.repository;
 
 import br.org.generation.crudfarmacia.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-    public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+    public List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
-
